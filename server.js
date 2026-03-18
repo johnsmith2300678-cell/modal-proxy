c25c83c3e3cc0356dad98a29d320dfa210821166
johnsmith2300678-cell <johnsmith2300678@gmail.com>
2026-03-18 14:09:14 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "https://api.us-west-2.modal.direct",
+    changeOrigin: true,
+    on: {
+      proxyReq: (proxyReq, req) => {
+        const auth = req.headers["authorization"];
+        if (auth) proxyReq.setHeader("Authorization", auth);
+      },
+    },
+  })
+);
+
+app.listen(process.env.PORT || 3000, () => console.log("Proxy running"));
