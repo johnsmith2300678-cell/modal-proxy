169705590b53e9e3ffaf5b0b5fc32e8d7978220a
johnsmith2300678-cell <johnsmith2300678@gmail.com>
2026-03-27 14:51:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,11 @@ app.use((req, res, next) => {
     if (req.body.temperature == null) req.body.temperature = 0.92;
     if (req.body.top_p == null) req.body.top_p = 0.95;
     if (req.body.repetition_penalty == null) req.body.repetition_penalty = 1.08;
-        req.body.max_tokens = 2048;
+    if (req.body.repetition_penalty == null) req.body.repetition_penalty = 1.08;
+
+// ADD THESE TWO:
+req.body.max_tokens = 2048;
+req.body.max_new_tokens = 2048;
   }
 
   next();
